c4bad6a21a96c2a59c03ba5bc3cb3e0fe91911cb
Aman <aman82591@gmail.com>
2025-04-21 15:56:45 +0530

    Added main readme file.

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -138,7 +138,7 @@ function Login() {
             <div>
               <button
                 type="submit"
-                className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 onClick={loginUser}
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
