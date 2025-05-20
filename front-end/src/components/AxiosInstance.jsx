99a897e25541058da43fc66a899feaa32aedb3b9
Aman <aman82591@gmail.com>
2025-05-20 23:26:35 +0530

    Removed backend locahost from axiosInstance.

--- front-end/src/components/AxiosInstance.jsx
+++ front-end/src/components/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_HOST || "http://localhost:5001",
+  baseURL: import.meta.env.VITE_BACKEND_HOST,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
